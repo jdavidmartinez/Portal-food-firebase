58e916e99f33b6f1ec8533d7ff5f90d20c43ba9f
DaviD D <jdavid.martinez@gmail.com>
2021-06-30 12:29:28 -0500

    Section 17 - video 216 - Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,13 +4,19 @@ import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const [meals, setMeals ] = useState([]); 
+  const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
   useEffect(() => {
-    
     const fetchMeals = async () => {
-      const response = await fetch("https://portal-st-default-rtdb.firebaseio.com/meals.json");
-      const responseData = await response.json()
+      const response = await fetch(
+        "https://portal-st-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const loadedMeals = [];
 
@@ -19,20 +25,33 @@ const AvailableMeals = () => {
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
-          price: responseData[key].price
-        })
+          price: responseData[key].price,
+        });
       }
 
       setMeals(loadedMeals);
-      setIsLoading(false);   
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
-    return <section className={classes.MeaslLoading}>
-      <p>Loading... </p>
-    </section>
+    return (
+      <section className={classes.MeaslLoading}>
+        <p>Loading... </p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
   const mealsList = meals.map((meal) => (
     <MealItem
